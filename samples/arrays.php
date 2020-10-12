ad40e6d1e964da24e67e04e8f6d0840a2352bd90
andyvkon <vkontank1@gmail.com>
2020-10-12 16:43:38 +0300

    L04: Added arrays example

---
+++ samples/arrays.php
@@ -0,0 +1,64 @@
+<?php
+$a1 = ['test' ,'qwerty' , 123];
+$a2 = [2=> 'test', 3=> 'qwerty' , 4=> 123];
+$a3 = [
+    'car' => 'Subaru',
+    'helicopter' => 'JBC',
+    'Monitor' => 'ACER',
+];
+    $groups =
+     [
+        [
+            'id' => 1,
+            'name' => 'PHP Basics',
+            'students' => [
+                            [
+                            'name' => 'Maximilian',
+                            'age'  => 23,
+                            ],
+                            [
+                                'name' => 'Fletcher',
+                                'age'  => 30,
+                            ],
+                          ],
+        ],
+        [
+            'id' => 2,
+            'name' => 'JS Basics',
+            'students' => [
+                            [
+                                'name' => 'Simpson' ,
+                                'age' => 11,
+                            ],
+                            [
+                                'name' => 'Bernard ' ,
+                                'age' => 68,
+                            ],
+                          ],
+        ],
+    ];
+
+$groups[0]['students'][/*Здесь указываем индекс массива */] = [
+
+    'name' => 'Vasya Pupkin',
+    'age' =>  99,
+]; // топ метод добавления инфы в массив.
+
+array_push($groups[1]['students'],[
+    'name' => 'Marge Simpson',
+        'age' => 35 ,
+    ]
+); // эта ф-я эррей пуш работает медленно , в 2 раза хуже предыдущей
+    var_dump($groups);
+
+//    $menu =
+//        [
+//            [
+//                'url' => '/',
+//                'title' => 'Home',
+//            ],
+//            [
+//                'url' => '/about-us',
+//                'title' => 'Home',
+//            ]
+//        ],
