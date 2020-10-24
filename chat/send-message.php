cb78b3dd4a2ac4b79d28a08a10fa8bea5bbef447
andyvkon <vkontank1@gmail.com>
2020-10-24 13:58:41 +0300

    L08: HW Easy-level filter

--- chat/send-message.php
+++ chat/send-message.php
@@ -27,31 +27,10 @@ if (!$message = ('fuck' || 'shit' || 'asshole' || 'motherfucker' || 'fat' || 'st
     //$content = serialize($data);
     file_put_contents(__DIR__ . 'storage5', $content, FILE_APPEND);
 
-    //$uncensoredarray = require_once(__DIR__ . '/uncensored.php'); // включает файл один раз
-    //$uncensoredWords = implode(')|(', $uncensoredarray);  // implode — Объединяет элементы массива в строку
 
-    //if (preg_match("/({$uncensoredWords})/iu", $message)) {
-    //    var_dump( 'Uncensored Message');
-    //
-    //    $postsData = file_get_contents(__DIR__ . '/posts-pivot-base.json');
-    //    $postsExist = strlen($postsData) > 0;
-    //    $postDataArray = $postsExist ? json_decode($postsData, true) : [];
-    //    $recordId = $postsExist ? max(array_keys($postDataArray)) + 1 : 1;
-    //
-    //    $postDataArray[$recordId] =
-    //        [
-    //            'nickname' => $nickname,
-    //            'message' => $message,
-    //            'unixTimeCreated' => time(),
-    //            'parentPostId' => ($_POST['post_type'] === 'new_post') ? null : (int)$_POST['current_posts_list'],
-    //        ];
-
-    //    file_put_contents(__DIR__ . '/posts-pivot-base.json', json_encode($postDataArray));
-    //}
-    //else {
         header('Location: /chat/');
         exit;
-    //}
+
     }
 else {
    echo '<h3 style="margin-left: 30%;">Error: Message contain bad words , please refrase your message!</h3>';
