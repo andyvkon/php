f58a037a3145a370493207303d75cadc65171f37
andyvkon <vkontank1@gmail.com>
2020-11-05 15:16:40 +0200

    Index file modify

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:400,300,700">
 <!doctype html>
 <html lang="en">
 <head>
@@ -6,15 +8,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="css/bootstrap.css" >
+    <link rel="stylesheet" href="content/css/bootstrap.css" >
     <title>Hello, world!</title>
+
+
 </head>
+
 <body >
+
+</body>
 <!-- Optional JavaScript; choose one of the two! -->
 
 <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
-<script src="js/jquery-3.5.1.min.js" ></script>
-<script src="js/bootstrap.min.js" ></script>
+<script src="content/js/jquery-3.5.1.min.js" ></script>
+<script src="content/js/bootstrap.min.js" ></script>
 <!-- Option 2: jQuery, Popper.js, and Bootstrap JS
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
@@ -22,67 +29,75 @@
 -->
 <div class="ml-4 pl-4 mt-2 pt-2">
 <?php
+error_reporting(E_ALL);
 
+$ext =  [
+    'css' => '<i class="fab fa-css3-alt"></i>',
+    'php' => '<i class="fab fa-php"></i>',
+    'jpg' => '<i class="fas fa-file-image"></i>',
+    'jpeg' => '<i class="fas fa-file-image"></i>',
+    'png' => '<i class="fas fa-file-image"></i>',
+    'css.map' => '<i class="fab fa-css3"></i>',
+    'js' => '<i class="fab fa-js-square"></i>',
+    'folder' => '<i class="fas fa-folder-open"></i>',
+    'other' => '<i class="fas fa-file-code"></i>',
+    'txt' => '<i class="fas fa-comment-dots"></i>',
 
-    // отфильтровать массив и вернуть (return) в массив
-    // элементы которые:
-    // 1) не содержкут точку как первый символ
-    // 2) строки которые начинаются с 'docker' ===  (false)   Ложь (т.е. не начинаются с "докер")
-    $files = scandir(__DIR__);
-    $elements = array_filter($files, static function (string $element){
-    return $element[0] !== '.' && stripos ($element, 'docker' ) === false && // stripos - ищем позицию в $element(а именно ищем подстроку докер , (в строке $element)
-        stripos ($element, 'css' ) === false && stripos ($element, 'js' ) === false
-        && stripos ($element, 'img' ) === false && stripos ($element, 'Homework' ) === false ;
-} );
+];
 
-//var_dump($elements);
-$html ='';
-foreach ($elements as $element){
-    $rout = __DIR__ . 'index.php/' . $element;
+function scan($dir, $tab) {
+    global $ext;
 
-    if (is_dir($rout)) {
-        $dir = scandir($rout);
-        $Nestedelements = array_filter($dir, static function (string $element){
-            return $element[0] !== '.';
-        } );
+    $d = opendir($dir);
 
-        foreach ($Nestedelements as $nestedelement){
-            echo "<li><a href='/{$element}/{$nestedelement}'> {$element}/{$nestedelement}</a></li>";
+      while ( ($name = readdir($d))  !== false  ){
+
+        if ($name == '.' or $name == '..' or $name == (stripos ($name, 'docker' ))
+            or $name == (stripos ($name, 'js' )) or $name == (stripos ($name, 'css' ))
+            or $name == (stripos ($name, '.git' ))
+            or $name == (stripos ($name, 'chatstorage' )) or $name == (stripos ($name, 'content' ))
+            or $name == (stripos ($name, '.idea' ))) continue;
+
+        $path = $dir . '/' . $name;
+        if (is_dir($path) ) {
+            echo "<b>{$tab} {$ext['folder']} {$name}</b><br>";
+            scan ($path, $tab . "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" );
+        } else {
+            $path_parts = pathinfo($path);
+            if (isset($path_parts['extension'], $ext) ) {
+                echo "{$tab}{$ext[$path_parts['extension']]} <a target='_blank' href='/{$path}'> {$name}</a><br>";
+            }
+            else {
+                echo "{$tab}{$ext['other']}<a target='_blank' href='/{$path}'> {$name}</a><br>";
+            }
+
         }
-
-    }else {
-        $html .= "<li><a href='/{$element}'> {$element}</a></li>";
     }
+    closedir($d);
 }
 
+scan('./' , '');
 
-    function scan($folder, $space) {
-    $openFolder = opendir($folder);
-    while ( ($CurrentDirrectoryName = readdir($openFolder)) !== false ) {
-        if ($CurrentDirrectoryName == '.' or '..' ) continue;
-        if (is_dir($folder . '/' . $CurrentDirrectoryName)){
-            echo "{$space}[{$folder}]";
-        }
-    }
-    }
 ?>
-</div>
 
-<ul>
-    <?= $html ?>
-</ul>
+    <style>
 
-</body>
-</html>
 
-<style>
-    a:link {
-        color: #629CCC; /* Цвет ссылок */
-        padding: 2px; /* Поля вокруг текста */
-    }
-    a:hover {
 
-        color: #ffe; /* Цвет ссылки */
-    }
-    body {background: url(img/2.jpg);}
-</style>
+        a {
+            text-decoration: none;
+
+        }
+        a:link {
+            color:#0c5460; /* Цвет ссылок */
+            padding: 2px; /* Поля вокруг текста */
+        }
+        a:hover {
+
+            color: #17a2b8; /* Цвет ссылки */
+        }
+        body {background: url(/img/64.jpg);}
+
+
+    </style>
+
