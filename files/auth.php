7cbc79854fd5271ae22e8a5bd8ada09555eeee7c
andyvkon <vkontank1@gmail.com>
2020-11-15 18:07:40 +0200

    L14: Added php mysqli connection

--- files/auth.php
+++ files/auth.php
@@ -12,9 +12,32 @@ if (!$login || !$password) {
 
 $config = require __DIR__ . '/config.php';
 
-$users = $config['users'];
-$passwordHash = $users[$login] ?? null;
+$link = mysqli_connect(
+  $config['db']['host'],
+  $config['db']['user'],
+  $config['db']['password'],
+  $config['db']['db'],
+);
 
+if (!$link) {
+    echo "Ошибка: Невозможно установить соединение с MySQL." . PHP_EOL;
+    echo "Код ошибки errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Текст ошибки error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
+
+$sql = "SELECT * FROM users WHERE login = ? LIMIT 1";
+$stmt = mysqli_prepare($link, $sql);
+mysqli_stmt_bind_param($stmt, 's', $login);
+mysqli_stmt_execute($stmt);
+$result = mysqli_stmt_get_result($stmt);
+
+$user = mysqli_fetch_assoc($result);
+
+mysqli_free_result();
+
+
+$passwordHash = $user['password'] ?? null;
 if (!$passwordHash || !password_verify($password, $passwordHash)){
     exit('login or password is incorrect');
 }
--- files/config.php
+++ files/config.php
@@ -1,5 +1,7 @@
 <?php
+//var_dump(password_hash('test123', PASSWORD_BCRYPT));
 //var_dump(password_hash('tok219', PASSWORD_BCRYPT));
+
 
 return [
     'baseDir' => __DIR__ . '/storage',
@@ -7,5 +9,13 @@ return [
     'users' => [
     'admin123' => '$2y$10$uetnPoefetfmddgFgezmMuGvIlJZ8yVvEcWroOcnQevz1ckSKBWAi',
     'admin12' => '$2y$10$B1r482x4ZIaSOjvOrrzkXeGln498oDMZClQYuGv/j1tyZ5PLnMbsS',
-    ]
-];
+    ],
+
+    'db' => [
+        'host' => 'db',
+        'user' => 'andy_user',
+        'password' => 'andy_pwd',
+        'db' => 'andy_db',
+        ],
+ ];
+
